6a289228d18ca025522a5be68eab80e85d71b3c0
Axel Wienberg <axel@wuenschenswert.net>
2014-05-11 16:15:31 +0200

    top and bottom of plane.

--- src/js/jigsaw.js
+++ src/js/jigsaw.js
@@ -31,23 +31,21 @@ THREE.JigsawGeometry = function ( width, height, depth, widthSegments, heightSeg
 	var height_half = height / 2;
 	var depth_half = depth / 2;
 
-	buildPlane( 'x', 'y',   1,  -1, width, height, depth_half, 0 ); // pz
-	buildPlane( 'x', 'y',   1,   1, width, height, - depth_half, 0 ); // nz
+	buildPlane( width, height, depth_half, 4 ); // pz
+	buildPlane( width, height, - depth_half, 5 ); // nz
  	// buildPlane( 'z', 'y', - 1, - 1, depth, height, width_half, 0 ); // px
  	// buildPlane( 'z', 'y',   1, - 1, depth, height, - width_half, 1 ); // nx
  	// buildPlane( 'x', 'z',   1,   1, width, depth, height_half, 2 ); // py
  	// buildPlane( 'x', 'z',   1, - 1, width, depth, - height_half, 3 ); // ny
 
-	function buildPlane( u, v, udir, vdir, width, height, depth, materialIndex ) {
+	function buildPlane( width, height, depth, materialIndex ) {
 
-		var w, ix, iy,
+		var ix, iy,
 		gridX = scope.widthSegments,
 		gridY = scope.heightSegments,
 		width_half = width / 2,
 		height_half = height / 2,
 		offset = scope.vertices.length;
-
-	    w = 'z';
 
 		var gridX1 = gridX + 1,
 		gridY1 = gridY + 1,
@@ -55,16 +53,16 @@ THREE.JigsawGeometry = function ( width, height, depth, widthSegments, heightSeg
 		segment_height = height / gridY,
 		normal = new THREE.Vector3();
 
-		normal[ w ] = depth > 0 ? 1 : - 1;
+		normal.z = depth > 0 ? 1 : - 1;
 
 		for ( iy = 0; iy < gridY1; iy ++ ) {
 
 			for ( ix = 0; ix < gridX1; ix ++ ) {
 
 				var vector = new THREE.Vector3();
-				vector[ u ] = ( ix * segment_width - width_half ) * udir;
-				vector[ v ] = ( iy * segment_height - height_half ) * vdir;
-				vector[ w ] = depth;
+				vector.x = ix * segment_width - width_half;
+				vector.y = iy * segment_height - height_half;
+				vector.z = depth;
 
 				scope.vertices.push( vector );
 
@@ -76,7 +74,7 @@ THREE.JigsawGeometry = function ( width, height, depth, widthSegments, heightSeg
 
 			for ( ix = 0; ix < gridX; ix++ ) {
 
-                if (jigsaw[vdir > 0 ? iy : gridY-iy-1][ix]===' ')
+                if (jigsaw[gridY-1-iy][ix]===' ')
                   continue;
 
 				var a = ix + gridX1 * iy;
@@ -88,6 +86,11 @@ THREE.JigsawGeometry = function ( width, height, depth, widthSegments, heightSeg
 				var uvb = new THREE.Vector2( ix / gridX, 1 - ( iy + 1 ) / gridY );
 				var uvc = new THREE.Vector2( ( ix + 1 ) / gridX, 1 - ( iy + 1 ) / gridY );
 				var uvd = new THREE.Vector2( ( ix + 1 ) / gridX, 1 - iy / gridY );
+
+                if (depth > 0) {
+                  var t = b; b = d; d = t;
+                  var uvt = uvb; uvb = uvd; uvd = uvt;
+                }
 
 				var face = new THREE.Face3( a + offset, b + offset, d + offset );
 				face.normal.copy( normal );
